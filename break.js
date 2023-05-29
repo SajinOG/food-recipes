02667bc7639ae2dfcffec9ab7c148a5563aee738
sajin ogaga <snickogaga@gmail.com>
2023-05-29 18:12:47 -0400

    still work in progress

---
+++ break.js
@@ -0,0 +1,26 @@
+let grade = 'A'
+
+switch (grade) {
+  case "A":
+    console.log("excellent");
+    break;
+
+  case "B":
+    console.log("destinction");
+    break;
+
+  case "C":
+    console.log("good");
+    break;
+
+  case "D":58
+    console.log("pass");
+    break;
+
+  case "F":
+    console.log("fail");
+    break;
+
+    default:
+        console.log("grade, is not an alphabet")
+}
